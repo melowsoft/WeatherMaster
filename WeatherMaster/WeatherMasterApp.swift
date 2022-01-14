358e9a371cf4c4faae7f0193b35e2ab94bb14510
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-01-14 12:01:51 +0100

    Initial Commit

---
+++ WeatherMaster/WeatherMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMasterApp.swift
+//  WeatherMaster
+//
+//  Created by Tony on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
